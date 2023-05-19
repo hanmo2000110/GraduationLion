14e46cea209e02ddefbd17d2ac99de3c3b8edf50
윤지인 <dev.zixxin@gmail.com>
2023-05-19 22:36:16 +0900

    mypage 졸업가능? 버튼 제외 구현

--- lib/screen/mypage.dart
+++ lib/screen/mypage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'recommend.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class MyPage extends StatefulWidget {
   const MyPage({super.key});
@@ -19,6 +21,87 @@ class MyPageState extends State<MyPage> {
             children: <Widget>[
               profile('졸업사자'),
               currentState(45),
+
+              // 학기 별 수강 강의 목록
+              // 1학기
+              semesterWithAdd('1'),
+              courseView('AI 프로젝트 입문', courseDesc1),
+              divider(),
+              courseView('환경과 인간', courseDesc2),
+              divider(),
+              courseView('모바일 앱 개발', courseDesc2),
+              divider(),
+              courseView('Computer Vision', courseDesc1),
+
+              // 2학기
+              semesterWithAdd('2'),
+              courseView('공학프로젝트 기획', courseDesc1),
+              divider(),
+              courseView('환경과 인간', courseDesc2),
+              divider(),
+              courseView('모바일 앱 개발', courseDesc2),
+              divider(),
+              courseView('Operating System', courseDesc1),
+
+              // 3학기
+              semesterWithAdd('3'),
+              courseView('AI 프로젝트 입문', courseDesc1),
+              divider(),
+              courseView('모바일 앱 개발', courseDesc2),
+              divider(),
+              courseView('환경과 인간', courseDesc2),
+              divider(),
+              courseView('Computer Network', courseDesc1),
+
+              // 4학기
+              semesterWithAdd('4'),
+              courseView('AI 프로젝트 입문', courseDesc1),
+              divider(),
+              courseView('환경과 인간', courseDesc2),
+              divider(),
+              courseView('모바일 앱 개발', courseDesc2),
+              divider(),
+              courseView('Computer Vision', courseDesc1),
+
+              // 5학기
+              semesterWithAdd('5'),
+              courseView('AI 프로젝트 입문', courseDesc1),
+              divider(),
+              courseView('환경과 인간', courseDesc2),
+              divider(),
+              courseView('모바일 앱 개발', courseDesc2),
+              divider(),
+              courseView('Computer Vision', courseDesc1),
+
+              // 6학기
+              semesterWithAdd('6'),
+              courseView('공학프로젝트 기획', courseDesc1),
+              divider(),
+              courseView('환경과 인간', courseDesc2),
+              divider(),
+              courseView('모바일 앱 개발', courseDesc2),
+              divider(),
+              courseView('Operating System', courseDesc1),
+
+              // 7학기
+              semesterWithAdd('7'),
+              courseView('AI 프로젝트 입문', courseDesc1),
+              divider(),
+              courseView('모바일 앱 개발', courseDesc2),
+              divider(),
+              courseView('환경과 인간', courseDesc2),
+              divider(),
+              courseView('Computer Network', courseDesc1),
+
+              // 8학기
+              semesterWithAdd('8'),
+              courseView('AI 프로젝트 입문', courseDesc1),
+              divider(),
+              courseView('환경과 인간', courseDesc2),
+              divider(),
+              courseView('Computer Vision', courseDesc1),
+              divider(),
+              courseView('환경과 인간', courseDesc2),
             ],
           ),
         ],
@@ -129,6 +212,97 @@ Widget currentState(int state) {
           ),
         ),
       ],
+    ),
+  );
+}
+
+Widget semesterWithAdd(String semester) {
+  // 학기 수 + 추가 버튼
+  return Stack(
+    alignment: AlignmentDirectional.center,
+    children: [
+      Container(
+        height: 30,
+        color: const Color(0xFFEFEFF4),
+      ),
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 24),
+            child: Text('$semester학기', style: const TextStyle(fontSize: 10)),
+          ),
+          Container(
+            height: 19,
+            width: 45,
+            margin: const EdgeInsets.only(left: 10),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  elevation: 0.0,
+                  minimumSize: Size.zero,
+                  padding: EdgeInsets.zero,
+                  backgroundColor: const Color(0xff8B95A1),
+                  textStyle: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 12)),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.add,
+                    size: 14,
+                    color: Colors.white,
+                  ),
+                  Text("추가",
+                      style: TextStyle(
+                          height: 1,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 10)),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+Widget courseView(String title, List desc) {
+  var showList = desc.join(", ");
+
+  return Slidable(
+    endActionPane: ActionPane(
+      extentRatio: 0.2,
+      motion: const ScrollMotion(),
+      children: [
+        SlidableAction(
+          onPressed: (BuildContext context) {},
+          backgroundColor: const Color(0xFFFF0000),
+          foregroundColor: Colors.white,
+          icon: Icons.delete,
+        ),
+      ],
+    ),
+    child: ListTile(
+      visualDensity: const VisualDensity(vertical: -3),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 24),
+      leading: Text(
+        title,
+        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+      ),
+      trailing: Text(
+        showList,
+        textAlign: TextAlign.end,
+        style: const TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 14,
+            color: Color(0xff8B95A1),
+            height: 1.1),
+      ),
     ),
   );
 }
